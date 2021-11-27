e69fa623170edbdf9f645ed20251b64be1e01954
HelLuv <works.person@gmail.com>
2021-11-27 10:04:04 +0300

    apollo server express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,32 @@
 import {MikroORM} from "@mikro-orm/core";
-import {Post} from "./entities/Post";
+import express from "express";
 import mikroConfig from './mikro-orm.config';
-import {__prod__} from "./constants";
+import {APP_PORT} from "./constants";
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, {title: 'My first post'});
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts)
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    })
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app})
+
+  app.get('/', ((_, res) => {
+    res.send('hello');
+  }))
+  app.listen(APP_PORT, () => {
+    console.log(`server started at localhost:${APP_PORT}`);
+  })
 }
 main().catch((err) => console.error(err));
 
